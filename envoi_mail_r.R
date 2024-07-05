c5429a4966a2b6968c424162d781428cfc72452e
kouloucrepin <108527731+kouloucrepin@users.noreply.github.com>
2024-07-05 02:14:26 +0200

    premier depot

---
+++ envoi_mail_r.R
@@ -0,0 +1,30 @@
+library(emayili)
+library(rmarkdown)
+
+render("doc_setup.qmd", output_format = "html_document")
+
+smtp <- server(
+  host = "smtp.gmail.com",
+  port = 587,
+  username = Sys.getenv("GMAIL_USERNAME"),
+  password = Sys.getenv("GMAIL_PASSWORD")
+)
+
+
+
+my_rmd_message <- envelope() |>
+  from(Sys.getenv("GMAIL_USERNAME")) |>
+  to(Sys.getenv("GMAIL_USERNAME"),"crepinkoulo@gmail.com") |>
+  subject("Email from an R Markdown file") |>
+  render("doc_setup.html")
+
+
+
+smtp(my_rmd_message, verbose = TRUE)
+
+
+
+
+
+
+

a28b65eecad0dd34c4d4fbc3f4eb29db14ccbfe0
kouloucrepin <108527731+kouloucrepin@users.noreply.github.com>
2024-07-05 04:36:55 +0200

    Update envoi_mail_r.R
    commit detection of error

--- envoi_mail_r.R
+++ envoi_mail_r.R
@@ -2,7 +2,7 @@ library(rmarkdown)
 render("doc_setup.qmd", output_format = "html_document")
 library(emayili)
 
-
+print(Sys.getenv("GMAIL_USERNAME"))
 smtp <- server(
   host = "smtp.gmail.com",
   port = 587,
@@ -10,7 +10,7 @@ smtp <- server(
   password = Sys.getenv("GMAIL_PASSWORD")
 )
 
-
+print(Sys.getenv("GMAIL_USERNAME"))
 
 my_rmd_message <- envelope() |>
   from(Sys.getenv("GMAIL_USERNAME")) |>
@@ -18,7 +18,7 @@ my_rmd_message <- envelope() |>
   subject("Email from an R Markdown file") |>
   render("doc_setup.html")
 
-
+print(Sys.getenv("GMAIL_USERNAME"))
 
 smtp(my_rmd_message, verbose = TRUE)
 
